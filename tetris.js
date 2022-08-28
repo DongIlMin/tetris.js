7a09c36ad2f3f2ef3f9d3f6b22a9d792eb1df382
DongilMin <tasabi0826@gmail.com>
2022-08-28 20:19:19 +0900

    tired..

--- tetris.js
+++ tetris.js
@@ -1,7 +1,41 @@
 const playground = document.querySelector(".playground > ul");
-console.log(playground);
 
-for (let i=0; i<20; i++) {
+// setting
+const GAME_LOWS = 20;
+const GAME_COL = 10;
+
+//variables
+let score = 50;
+let duration = 500;
+let downInterval;
+let tempMovingItem; 
+
+const BLOCKS = {
+    tree:[
+        [[0,0], [0,1], [1,0], [1,1]],
+        [[1,2], [5,6], [6,6], [7,7]],
+        [],
+        []
+    ]
+};
+
+const movingItem = {
+    type: "tree",
+    direction: 0,
+    top:0,
+    left:0,
+};
+init()
+
+function init() {
+    tempMovingItem =  {...movingItem};
+    for (let i=0; i<20; i++) {
+        prependNewLine();
+    }
+    renderBlocks()
+}
+
+function prependNewLine() {
     const li = document.createElement("li");
     const ul = document.createElement("ul");
     for(let j=0; j<10; j++){
@@ -10,4 +44,12 @@ for (let i=0; i<20; i++) {
     }
     li.prepend(ul);
     playground.prepend(li);
+}
+function renderBlocks() {
+    const { type, direction, top, left} = tempMovingItem;
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0];
+        const y = block[1];
+        console.log(tempMovingItem);
+    })
 }
